bd149bc6c65adab3a1b0493127eb93c20bc9802b
Maxime <maximecattet@gmx.com>
2016-05-16 17:59:11 +0200

    fix farmer tests

--- test/farmer.specs.js
+++ test/farmer.specs.js
@@ -1,16 +1,15 @@
 var api = require('./api');
-var expect = require('char').expect;
+var expect = require('chai').expect;
 
 describe('api', function(){
     describe('#farmer', function(){
         var farmerID = 0;
         var farmerCountry = '';
 
-        it('should get user from current token', function(){
+        it('should get user from current token', function(done){
             api.Farmer.getFromCurrentToken(function(err, res){
                 expect(err).to.not.be.ok;
                 expect(res).to.be.ok;
-                //TODO: check if object is right;
                 farmerID = res.farmer.id;
                 farmerCountry = res.farmer.country;
 
@@ -18,7 +17,7 @@ describe('api', function(){
             });
         });
 
-        it('should change country to U.S', function(){
+        it('should change country to U.S', function(done){
             api.Farmer.changeCountry('us', function(err, res){
                 expect(err).to.not.be.ok;
                 expect(res).to.be.ok;
@@ -27,7 +26,7 @@ describe('api', function(){
             });
         });
 
-        it('should change country back to farmer\'s one', function(){
+        it('should change country back to farmer\'s one', function(done){
             api.Farmer.changeCountry(farmerCountry, function(err, res){
                 expect(err).to.not.be.ok;
                 expect(res).to.be.ok;
@@ -35,15 +34,15 @@ describe('api', function(){
             });
         });
 
-        it('should register to tournament', function(){
-            api.Farmer.registerTournament(function(err, res)(
+        it('should register to tournament', function(done){
+            api.Farmer.registerTournament(function(err, res){
                 expect(err).to.not.be.ok;
                 expect(res).to.be.ok;
                 done();
-            ));
+            });
         });
 
-        it('should unregister to tournament', function(){
+        it('should unregister to tournament', function(done){
             api.Farmer.unregisterTournament(function(err, res){
                 expect(err).to.not.be.ok;
                 expect(res).to.be.ok;
@@ -51,24 +50,27 @@ describe('api', function(){
             });
         });
 
-        it('should get connected farmers', function(){
+        it('should get connected farmers', function(done){
             api.Farmer.getConnected(function(err, res){
                 expect(err).to.not.be.ok;
                 expect(res).to.be.ok;
-                //TODO: add expect array
+                expect(res.farmers).to.be.a('array');
                 done();
             });
         });
 
-        it('should get users with ID ' + farmerID, function(){
+        it('should get current user with ID', function(done){
             api.Farmer.get(farmerID, function(err, res){
                 expect(err).to.not.be.ok;
                 expect(res).to.be.ok;
+                expect(res.farmer.id).to.equal(farmerID);
+                expect(res.farmer.country).to.equal(farmerCountry);
+
                 done();
             });
         });
 
-        it('should update farmer', function(){
+        it('should update farmer', function(done){
             api.Farmer.update(function(err, res){
                 expect(err).to.not.be.ok;
                 expect(res).to.be.ok;
